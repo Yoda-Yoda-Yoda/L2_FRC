0c8f05e84c31fd177c0b7e8db1d3705deb0039db
Yoda-Yoda-Yoda <chambersl3255@masseyhigh.school.nz>
2026-03-10 12:27:04 +1300

    Completed FRC

---
+++ B_01_FRC_v2.py
@@ -0,0 +1,335 @@
+import pandas
+from numpy.ma.core import minimum
+from tabulate import tabulate
+from datetime import date
+import math
+
+
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decoration
+    at the start and end"""
+
+    return f"{decoration * 3} {statement} {decoration * 3}"
+
+def yes_no_check(question):
+    """Checks that users entered yes / y or no / n to a question"""
+
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Please enter yes (y) or no (n).\n")
+
+def instructions():
+    print("Instructions", "ℹ️")
+
+    print('''
+Instructions 🤣🤣🤣🤣
+
+    ''')
+
+def not_blank(question):
+    """Checks that a user input isn't blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry! this can't be blank. Please try again\n")
+
+def num_check(question, num_type="float", exit_code=None):
+    """Checks that response is a float / integer """
+
+    if num_type == "float":
+        error = "Please enter a number more then 0"
+    else:
+        error = "Please enter an integer more then 0"
+
+    while True:
+        response = input(question)
+
+        # checks for exit code and return it if entered
+        if response == exit_code:
+            return response
+
+        # check datatype is correct and that number
+        # is more than zero
+        try:
+
+            if num_type == "float":
+                response = float(response)
+            else:
+                response = int(response)
+
+            if response > 0:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+def get_expenses(exp_type, how_many=1):
+    """Get variable / fixed expenses and outputs
+    panda (as a string) and a subtotal of the expenses"""
+
+    # Lists for panda
+    all_items = []
+    all_amounts = []
+    all_dollar_per_item = []
+
+
+    # Expenses dictionary
+    expenses_dict = {
+        "Item": all_items,
+        "Amount": all_amounts,
+        "$ / Item": all_dollar_per_item
+    }
+
+    amount = how_many  # how_many defaults to 1
+    how_much_question = "How much? $"
+
+    # Loop to get expenses
+    while True:
+        # Get item name and check it's not blank
+        item_name = not_blank("Item Name: ")
+
+        # check users enter at least one variable expense
+
+        if ((exp_type == "variable" and item_name == "xxx")
+                and len(all_items) == 0):
+            print("Oops - you have not entered anything. "
+                  "You need at least one item.")
+        elif item_name == "xxx":
+            break
+
+        if exp_type == "variable":
+            amount = num_check(f"How many <enter for how {how_many}>", "int", "")
+
+            if amount == "":
+                amount = how_many
+
+            how_much_question = "Price for one? $"
+
+        # get price for item (question customised depending on expense type)
+        price_for_one = num_check(how_much_question, "float")
+
+        all_items.append(item_name)
+        all_amounts.append(amount)
+        all_dollar_per_item.append(price_for_one)
+
+    # make panda
+    expense_frame = pandas.DataFrame(expenses_dict)
+
+    # calculate row cost
+    expense_frame['Cost'] = expense_frame['Amount'] * expense_frame['$ / Item']
+
+    # calculate subtotal
+    subtotal = expense_frame['Cost'].sum()
+
+    # Apply currency formatting to currency columns.
+    add_dollars = ['Amount', '$ / Item', 'Cost']
+    for var_item in add_dollars:
+        expense_frame[var_item] = expense_frame[var_item].apply(currency)
+
+    if exp_type == "variable":
+        expanse_string = tabulate(expense_frame, headers='keys',
+                                  tablefmt='psql', showindex=False)
+    else:
+        expanse_string = tabulate(expense_frame[['Item', 'Cost']], headers='keys',
+                                  tablefmt='psql', showindex=False)
+    return expanse_string, subtotal
+
+def currency(x):
+    """Formats numbers as currency ($#.##"""
+    return "${:.2f}".format(x)
+
+def profit_goal(total_cost):
+    """Calculate profit goal work out profit goal and total sales required"""
+    # Initialise variable and error message
+    error = "Please enter a valid profit goal\n"
+
+    valid = False
+    while not valid:
+        # ask for profit goal...
+        response = input("What is your profit goal (eg $500 or 50%): ")
+
+        # check if first character is $...
+        if response[0] == "$":
+            profit_type = "$"
+            # get amount (everything after the $)
+            amount = response[1:]
+
+            # check if the last character is %
+        elif response[-1] == "%":
+            profit_type = "%"
+            # get amount (everything before the %)
+            amount = response[:-1]
+
+        else:
+            # set response to amount for now
+            profit_type = "unknown"
+            amount = response
+
+        try:
+            # check amount is a number more than zero.
+            amount = float(amount)
+            if amount <= 0:
+                print(error)
+                continue
+        except ValueError:
+            print(error)
+            continue
+
+        if profit_type == "unknown" and amount >= 100:
+            dollar_type = yes_no_check(f"Do you mean ${amount:.2f}. ie {amount:.2f} dollars")
+
+            # set profit type based on user answer above
+            if dollar_type == "yes":
+                profit_type = "$"
+            else:
+                profit_type = "%"
+
+        elif profit_type == "unknown" and amount < 100:
+            precent_type = yes_no_check(f"Do you mean {amount}%, y / n:")
+            if precent_type == "yes":
+                profit_type = "%"
+            else:
+                profit_type = "$"
+
+        if profit_type == "$":
+            return amount
+
+        else:
+            goal = (amount / 100) * total_cost
+            return goal
+
+def round_up(amount, round_val):
+    """Rounds amount to the desired whole number"""
+    return int(math.ceil(amount / round_val)) * round_val
+
+
+# main routine starts here
+
+# initialise variables...
+
+# assume we have no fixed expenses for now
+fixed_subtotal = 0
+fixed_panda_string = ""
+
+print(make_statement("Fund Raising Calculator", "💰"))
+
+print()
+want_instructions = yes_no_check("Do you want to see the instructions? ")
+print()
+
+if want_instructions == "yes":
+    instructions()
+
+print()
+
+#Get product details...
+product_name = not_blank("Product Name:  ")
+quantity_made = num_check("Quantity being made: ", "integer")
+
+# Get variable expenses...
+print("Let's get the variable expenses....")
+variable_expenses = get_expenses("variable", quantity_made)
+
+variable_panda_string = variable_expenses[0]
+variable_subtotal = variable_expenses[1]
+
+# ask user if they have fixed expenses and retrieve them
+print()
+has_fixed = yes_no_check("Do you have fixed expenses? ")
+
+if has_fixed == "yes":
+    fixed_expenses = get_expenses("fixed")
+
+    fixed_panda_string = fixed_expenses[0]
+    fixed_subtotal = fixed_expenses[1]
+
+    # if the user has not entered any expenses,
+    # Set empty panda to "" so that it does not display!
+    if fixed_subtotal == 0:
+        has_fixed = "no"
+        fixed_panda_string = ""
+
+total_expenses = variable_subtotal + fixed_subtotal
+total_expenses_string = f"Total Expenses: ${total_expenses:.2f}"
+
+# Get profit Goal here.
+target = profit_goal(total_expenses)
+sales_target = total_expenses + target
+
+# Calculate minimum selling price and rounding
+# it to nearest desired dollar amount
+selling_price = (total_expenses + target) / quantity_made
+round_to = num_check("Round To: ", "integer")
+suggested_price = round_up(selling_price, round_to)
+# strings / output area
+
+# **** Get current date for heading and file name ****
+today = date.today()
+
+# Get day, month and year as individual strings
+day = today.strftime("%d")
+month = today.strftime("%m")
+year = today.strftime("%Y")
+
+# Heading / Strings...
+main_heading_string = make_statement(f"Fund Raising Calculator "
+                                     f"({product_name}, {day}/{month}/{year})", "=")
+
+quantity_string = f"Quantity being made: {quantity_made}"
+variable_heading_string = make_statement("Variable Expenses", "-")
+variable_subtotal_string = f"Variable Expenses Subtotal: ${variable_subtotal:.2f}"
+
+# set up string if we have fixed costs
+if has_fixed == "yes":
+    fixed_heading_string = make_statement("Fixed Expenses", "-")
+    fixed_subtotal_string = f"Fixed Expenses Subtotal: {fixed_subtotal:.2f}"
+
+# set fixed cost string to blank if we don't have fixed costs
+else:
+    fixed_heading_string = make_statement("You have no Fixed Expenses", "-")
+    fixed_subtotal_string = "Fixed Expenses Subtotal: $0.00"
+
+selling_price_heading = make_statement("Selling Price Calculations", "-")
+profit_goal_string = f"Profit Goal: ${target:.2f}"
+sales_target_string = f"\nTotal Sales Needed: ${sales_target:.2f}"
+
+minimum_price_string = f"Minimum Selling Price: ${selling_price:.2f}"
+suggested_price_string = make_statement(f"Suggested Selling Price: "
+                                        f"${suggested_price:.2f}", "*")
+
+# list of string to be outputted / written to file
+to_write = [main_heading_string,"\n", quantity_string,
+            "\n", variable_heading_string, "\n", variable_panda_string, "\n",
+            variable_subtotal_string,
+            "\n", fixed_heading_string, "\n", fixed_panda_string, "\n",
+            fixed_subtotal_string,
+            "\n",selling_price_heading, "\n", total_expenses_string,
+            "\n", profit_goal_string, "\n", sales_target_string, "\n",
+            minimum_price_string, "\n", suggested_price_string
+            ]
+
+# print area
+print()
+for item in to_write:
+    print(item)
+
+# create file to hold data (add .txt extension)
+file_name = f"{product_name}_{year}_{month}_{day}"
+write_to = "{}.txt".format(file_name)
+
+text_file = open(write_to, "w+")
+
+for item in to_write:
+    text_file.write(item)
